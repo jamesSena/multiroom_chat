891f50c327924fddef5da1c561f0a0068be3b675
Jamesson Sena <jsena@coddera.com>
2018-10-11 09:34:46 -0300

    trabalhando com socket

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ var app = require('./config/server');
 /**
  * Parametrizar a porta de serviço
  */
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
     console.log('Servidor Online');
 })
+
+var io = require('socket.io').listen(server);
+app.set('io',io);
+//criar a conexao por websocket
+io.on('connection', function(socket){
+    console.log('Usuario conectou!!');
+    socket.on('disconnect', function(){
+        console.log('Usuario desconectou!!');
+    });
+});
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,14 @@
 module.exports.iniciaChat = function(application,req,res){
+    var dadosForm = req.body;
+    console.log(dadosForm);
+    req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty;
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3,15);
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('index',{validacao:erros});
+        return;
+    }
+    application.get('io').emit('msgParaClient')
     res.render("chat");
 }
